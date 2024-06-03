3738e359bcc4cb759009806b332d9f7121f65d67
kunalg3 <110518293+kunalg3@users.noreply.github.com>
2024-06-03 23:06:26 +0530

    added cors origin

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT=process.env.PORT||8000
 
 app.use(express.json())
 app.use(cors({
-  origin:'http://localhost:3000',
+  origin:'https://quiz-app-client-self.vercel.app/',
   credentials:true
 }))
 
